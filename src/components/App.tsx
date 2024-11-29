5f60bdee5963e6b3b64f2f1c6ebec7ddc35124d8
Spencer Kingman <spencer.kingman@niche.com>
2024-11-29 18:28:49 -0500

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,51 @@
+import 'styles/index.css'
+import 'assets/fonts/Zen_Old_Mincho/ZenOldMincho-Regular.ttf'
+
+function App() {
+  return (
+    <div className="zen-old-mincho-regular flex min-h-screen flex-col text-gray-900">
+      {/* <header className="flex-initial">
+        <span className="p-10">spencerkingman.com</span>
+      </header> */}
+      <main className="flex-auto p-12 subpixel-antialiased">
+        <h1 className="pb-3 text-4xl tracking-tight">Spencer Kingman</h1>
+        <ul className="list-circle list-inside text-xl">
+          <li className="m-1  p-2 pl-0">
+            <a
+              className="w-64 p-2 pl-0"
+              href="https://spenking.spencerkingman.com"
+            >
+              songs
+            </a>
+          </li>
+          <li className="m-1   p-2 pl-0">
+            <a
+              className="w-64 p-2 pl-0"
+              href="https://personal-page.spencerkingman.com"
+            >
+              diary
+            </a>
+          </li>
+          <li className="m-1  p-2 pl-0">
+            <a
+              className="w-64 p-2 pl-0"
+              href="https://personal-page.spencerkingman.com"
+            >
+              lists
+            </a>
+          </li>
+          <li className="m-1   p-2 pl-0">
+            <a
+              className="w-64 p-2 pl-0"
+              href="https://personal-page.spencerkingman.com"
+            >
+              art
+            </a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  )
+}
+
+export default App
