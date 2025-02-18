952930f6c4ac22f101719999d07997ec2db17a27
Spencer Kingman <spencer.kingman@niche.com>
2025-02-17 19:17:51 -0500

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  base: '/home-page/',
+  base: '/',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
