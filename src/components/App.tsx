78cc04af17c6d1e48675e0d7ab5766430bc0f3d1
Spencer Kingman <spencer.kingman@niche.com>
2025-09-29 22:23:53 -0400

    active events on links

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,47 +4,44 @@ import 'assets/fonts/Zen_Old_Mincho/ZenOldMincho-Regular.ttf'
 function App() {
   return (
     <div className="zen-old-mincho-regular flex min-h-screen flex-col text-gray-900">
-      {/* <header className="flex-initial">
-        <span className="p-10">spencerkingman.com</span>
-      </header> */}
       <main className="flex-auto p-12 subpixel-antialiased">
         <h1 className="pb-3 text-4xl tracking-tight">Spencer Kingman</h1>
         <ul className="list-circle list-inside text-xl">
           <li className="m-1  p-2 pl-0">
             <a
-              className="w-64 p-2 pl-0"
+              className="w-64 p-2 pl-0 active:text-gray-500 active:drop-shadow-[6px_8px_1px_gray-200]"
               href="https://spenking.spencerkingman.com"
             >
               songs
             </a>
           </li>
-          <li className="m-1   p-2 pl-0">
+          <li className="m-1 p-2 pl-0">
             <a
-              className="w-64 p-2 pl-0"
+              className="w-64 p-2 pl-0 active:text-gray-500 active:drop-shadow-[6px_8px_1px_gray-200]"
               href="https://personal-page.spencerkingman.com"
             >
               music diary
             </a>
           </li>
-          <li className="m-1   p-2 pl-0">
+          <li className="m-1 p-2 pl-0">
             <a
-              className="w-64 p-2 pl-0"
+              className="w-64 p-2 pl-0 active:text-gray-500 active:drop-shadow-[6px_8px_1px_gray-200]"
               href="https://professional.spencerkingman.com"
             >
               professional
             </a>
           </li>
-          <li className="m-1  p-2 pl-0">
+          <li className="m-1 p-2 pl-0">
             <a
-              className="pointer-events-none w-64 p-2 pl-0 line-through"
+              className="pointer-events-none w-64 p-2 pl-0 line-through decoration-2"
               href="#"
             >
               lists
             </a>
           </li>
-          <li className="m-1   p-2 pl-0">
+          <li className="m-1 p-2 pl-0">
             <a
-              className="pointer-events-none w-64 p-2 pl-0 line-through"
+              className="pointer-events-none w-64 p-2 pl-0 line-through decoration-2"
               href="#"
             >
               art
